849e161f1213cc9ddefee11f0bf4e96148afc3fb
Chandima Wijerathna <153199095+Chandima0406@users.noreply.github.com>
2025-08-05 00:34:51 +0530

    fs and fs/promises

--- index.js
+++ index.js
@@ -1,17 +1,88 @@
-//const os = require('os');
-//const path = require('path');
+import { log, error } from "node:console";
+//import { appendFile, readFile, rm } from "node:fs";
+import { readFile, appendFile, rmdir, writeFile } from "node:fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+//import { writeFile } from "node:fs";
+import { Buffer } from "buffer";
 
-import os from 'os';
-import path, { join } from 'path';
-import { readFile } from 'fs';
-import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 
-readFile(join(__dirname, 'myname.txt'),(err, data) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(data.toString());
-});
+/// promise API example
+// readFile
+readFile(join(__dirname, "read.txt"), { encoding: "utf-8" })
+  .then((data) => {
+    log("File content:", data);
+  })
+  .catch((err) => {
+    error(err);
+  });
 
+  // appendFile
+  appendFile(join(__dirname, "append.txt"), "Hello Node.js", { encoding: "utf-8" })
+    .then(() => {
+      log("File appended successfully.");
+    })
+    .catch((err) => {
+      error(err);
+    });
+
+  //writeFile
+  writeFile(join(__dirname, "write.txt"), "Hello Node.js", { encoding: "utf-8" })
+    .then(() => {
+      log("File written successfully.");
+    })
+    .catch((err) => {
+      error(err);
+    });
+
+/////// callcack API example
+// readFile, writeFile, appendFile, rm
+
+// readFile(join(__dirname, "read.txt"), { encoding: "utf-8" }, (err, data) => {
+//   if (err) {
+//     log("Error reading file:", err);
+//     return;
+//   }
+//   log("File content:", data);
+// });
+
+// writeFile(join(__dirname, "read.txt"), "my name is chandima.......", (err) => {
+//   if (err) {
+//     log("Error writing file:", err);
+//     return;
+//   }
+//   //log("File written successfully.");
+// });
+
+// const data = new Uint8Array(Buffer.from("Hello Node.js"));
+// writeFile("message.txt", data, (err) => {
+//   if (err) {
+//     error("Error writing file:", err);
+//     return;
+//   }
+
+//   //log("File written successfully.");
+// });
+
+
+// // append using join
+// appendFile(join(__dirname, "message.txt"), "\nHello !", (err) => {
+//   if (err) {
+//     error("Error appending to file using join:", err);
+//     return;
+//   }
+
+//   log("File appended successfully using join.");
+// });
+ 
+// // remove using join
+// rm(join(__dirname, "message.txt"), (err) => {
+//   if (err) {
+//     error("Error removing file using join:", err);
+//     return;
+//   }
+
+//   log("File removed successfully using join.");
+// });
--- test.js
+++
@@ -1,4 +0,0 @@
-import main from './myfunctions.js'; // Importing the main object from myfunctions.js
-console.log(main.math(2, 3, 4, 5));  // Testing the math function from the imported object
-
-
